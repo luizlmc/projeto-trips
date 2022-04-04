b0dc5a10fa3ba7f8bab422e0b8d21e680ba0a198
Luiz Carvalho <luiz.carvalho.lmc@gmail.com>
2022-04-04 20:13:58 -0300

    Styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { MdFlightTakeoff }  from 'react-icons/md';
+
 import api from '../../services/api';
+
+import './styles.css';
 
 function Home() {
 
@@ -10,8 +14,6 @@ function Home() {
         async function loadApi(){
             const response = await api.get('trips');
             setTrips(response.data);
-
-            console.log(response.data);
         }
 
         loadApi();
@@ -19,7 +21,25 @@ function Home() {
 
     return(
         <div>
-            <h1>Home</h1>
+            <div className='box'>
+                {trips.map(trip => (
+                    <li key={trip.id}>
+                        <img src={trip.image} alt={trip.title} />
+                        <strong>{trip.title}</strong>
+                        <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+
+                        <button 
+                        type="button"
+                        onClick={() => {}}
+                        >
+                            <div>
+                                <MdFlightTakeoff size={16} color="#FFF" />
+                            </div>
+                            <span>SOLICITAR RESERVA</span>
+                        </button>
+                    </li>
+                ))}
+            </div>
         </div>
     )
 }
